708c7d474f75f4579b82ce61ce587c36af5669e8
Lukáš Loukota <lukasloukota94@gmail.com>
2018-11-24 15:49:00 +0100

    Define a TemplateMatching class

--- template_matching/api.py
+++ template_matching/api.py
@@ -2,29 +2,31 @@ import cv2
 import numpy as np
 
 
-def match_template(file_path, template_path):
-    """
 
-    :param file_path:       path to the main file (screenshot of a website)
-    :param template_path:   path to the template (logo on a website, ..., the element to be found)
-    :return:
-    """
-    # cv2.namedWindow("imageWindow")
-    img_rgb = cv2.imread(file_path)
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+class TemplateMatching(object):
+    def match_template(self, file_path, template_path):
+        """
 
-    template = cv2.imread(template_path, 0)
-    # w, h = template.shape[::-1]
+        :param file_path:       path to the main file (screenshot of a website)
+        :param template_path:   path to the template (logo on a website, ..., the element to be found)
+        :return:
+        """
+        # cv2.namedWindow("imageWindow")
+        img_rgb = cv2.imread(file_path)
+        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
+        template = cv2.imread(template_path, 0)
+        # w, h = template.shape[::-1]
 
-    loc = np.where(res >= threshold)
+        res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+        threshold = 0.8
 
-    if len(loc[0]) > 0:
-        return True
+        loc = np.where(res >= threshold)
 
-    return False
+        if len(loc[0]) > 0:
+            return True
+
+        return False
 # if __name__ == "__main__":
 #     match_template("website.png", "logo.png")
 #     match_template("website.png", "bad.png")
